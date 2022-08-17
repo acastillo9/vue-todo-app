3ce352d97b1d0c95bfdc6693c8c330c5ad39a6d9
Andrés Castillo <acastillo119@gmail.com>
2022-08-17 07:47:55 -0500

    sandbox version

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,31 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 
 // give each todo a unique id
-let id = 0
+let id = 0;
 
-const newTodo = ref('')
+const newTodo = ref("");
 const todos = ref([
-  { id: id++, text: 'Learn HTML' },
-  { id: id++, text: 'Learn JavaScript' },
-  { id: id++, text: 'Learn Vue' }
-])
+  { id: id++, text: "Aprender HTML" },
+  { id: id++, text: "Aprender JavaScript" },
+  { id: id++, text: "Aprender Vue" },
+]);
 
 function addTodo() {
-  todos.value.push({ id: id++, text: newTodo.value })
-  newTodo.value = ''
+  todos.value.push({ id: id++, text: newTodo.value });
+  newTodo.value = "";
 }
 
 function removeTodo(todo) {
-  todos.value = todos.value.filter((t) => t !== todo)
+  todos.value = todos.value.filter((t) => t !== todo);
 }
 </script>
 
 <template>
   <h1>ToDo App</h1>
   <form @submit.prevent="addTodo">
-    <input v-model="newTodo">
-    <button>Add Todo</button>    
+    <input v-model="newTodo" />
+    <button>Add Todo</button>
   </form>
   <ul>
     <li v-for="todo in todos" :key="todo.id">
