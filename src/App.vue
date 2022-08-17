32d71584fc254cdb299b999196362be12b662105
Andrés Castillo <acastillo119@gmail.com>
2022-08-17 14:23:06 -0500

    watchers sandbox

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,22 @@ import { ref, computed, onMounted, watch } from "vue";
 const newTodo = ref("");
 const hideCompleted = ref(false);
 const todos = ref([]);
-const selectedTodoId = ref(1)
-const selectedTodoData = ref(null)
+const selectedTodoId = ref(0);
+const selectedTodoData = ref(null);
 
 const filteredTodos = computed(() => {
-  return hideCompleted.value ? todos.value.filter((t) => !t.completed) : todos.value;
+  return hideCompleted.value
+    ? todos.value.filter((t) => !t.completed)
+    : todos.value;
 });
 
 async function addTodo() {
   const todo = { id: todos.value.length + 1, title: newTodo.value };
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/todos`,
-    { method: 'POST', body: JSON.stringify(todo) }
-  )
-  await res.json()
+  const res = await fetch(`https://jsonplaceholder.typicode.com/todos`, {
+    method: "POST",
+    body: JSON.stringify(todo),
+  });
+  await res.json();
 
   todos.value.push(todo);
   newTodo.value = "";
@@ -26,38 +28,36 @@ async function addTodo() {
 async function removeTodo(todo) {
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/todos/${todo.id}`,
-    { method: 'DELETE' }
-  )
-  await res.json()
+    { method: "DELETE" }
+  );
+  await res.json();
 
   todos.value = todos.value.filter((t) => t !== todo);
 }
 
 async function fetchTodos() {
   todos.value = [];
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/todos`
-  )
-  todos.value = await res.json()
+  const res = await fetch(`https://jsonplaceholder.typicode.com/todos`);
+  todos.value = await res.json();
 }
 
 function selectTodo(todoId) {
-  selectedTodoId.value = todoId
+  selectedTodoId.value = todoId;
 }
 
 async function fetchTodoData() {
-  selectedTodoData.value = null
+  selectedTodoData.value = null;
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/todos/${selectedTodoId.value}`
-  )
-  selectedTodoData.value = await res.json()
+  );
+  selectedTodoData.value = await res.json();
 }
 
-watch(selectedTodoId, fetchTodoData)
+watch(selectedTodoId, fetchTodoData);
 
 onMounted(() => {
-  fetchTodos()
-})
+  fetchTodos();
+});
 </script>
 
 <template>
@@ -70,7 +70,11 @@ onMounted(() => {
   <ul>
     <li v-for="todo in filteredTodos" :key="todo.id">
       <input type="checkbox" v-model="todo.completed" />
-      <span :class="['cursor-pointer', { done: todo.completed }]" @click="selectTodo(todo.id)">{{ todo.title }}</span>
+      <span
+        :class="['cursor-pointer', { done: todo.completed }]"
+        @click="selectTodo(todo.id)"
+        >{{ todo.title }}</span
+      >
       <button @click="removeTodo(todo)">X</button>
     </li>
   </ul>
